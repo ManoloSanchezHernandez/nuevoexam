7036d12a2ef6a5557d636bb1cff07c01d84abe0f
ManoloSanchezHernandez <2302084@utrivieramaya.edu.mx>
2024-10-17 22:12:19 -0500

    Examennuevo

---
+++ README.md
@@ -0,0 +1,1 @@
+Esta pagina web consiste en una pagina web que cuenta con elemento como lo puede ser un navbar, alider, footer, y cards, que son elementos fundamentales para cualquiera de estas.
---
+++ js/script2.js
@@ -0,0 +1,20 @@
+const value = document.querySelector('#range_value');
+const input = document.querySelector('#range');
+value.textContent = input.value;
+input.addEventListener ("input", (event) => {
+    value.textContent = event.target.value;
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navItems = document.querySelectorAll('.nav-item');
+
+    navItems.forEach((item, index) => {
+        setTimeout(() => {
+            item.classList.add('show');
+        }, 100 * index);
+    });
+});
+
+document.getElementById('year').textContent = new Date().getFullYear();
+
